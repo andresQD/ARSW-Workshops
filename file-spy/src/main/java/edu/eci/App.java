d1c3fff355782ec0421f719eb96d7a7dab524e9d
Andres Quintero <andres.quintero98@hotmail.com>
2020-01-17 18:45:28 -0500

    building life cycles and plugins

--- file-spy/src/main/java/edu/eci/App.java
+++ file-spy/src/main/java/edu/eci/App.java
@@ -1,13 +1,9 @@
 package edu.eci;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args )throws Exception
     {
-        System.out.println( "Hello World!" );
+        FileSpy.main(args);
     }
 }
